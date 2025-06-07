611fb363287a2653a3512570196329772dd4f107
Tychele Turner <tycheleturner@gmail.com>
2025-06-07 16:53:03 -0500

    [script] add info to link

--- flamingo_plot.R
+++ flamingo_plot.R
@@ -6,7 +6,7 @@ library(plotly)
 library(htmlwidgets)
 
 option_list <- list(
-	make_option(c('-i', '--input'), action='store', type='character', default='input.txt', help='.hic file to use as input'),
+	make_option(c('-i', '--input'), action='store', type='character', default='input.txt', help='output file from run_flamingo.R to use as input'),
         make_option(c('-o', '--output'), action='store', type='character', default='output', help='output file name prefix')
 )
 opt <- parse_args(OptionParser(option_list = option_list))
