118cc00bafd2a47b76d7825a00f1a36fb92e942c
Tychele Turner <tycheleturner@gmail.com>
2025-06-07 16:34:48 -0500

    [script] add script to run flamingo

---
+++ run_flamingo.R
@@ -0,0 +1,19 @@
+#!/bin/Rscript
+
+library(optparse)
+library(FLAMINGOrLite)
+
+option_list <- list(
+	make_option(c('-i', '--input'), action='store', type='character', default='inter_30.hic', help='.hic file to use as input'),
+	make_option(c('-r', '--chromosome'), action='store', type='character', default='chr1', help='Variant file created from the variant directory'),
+	make_option(c('-t', '--thread'), action='store', type='numeric', default=1, help='Number of threads that flamingo can use during the run')
+)
+opt <- parse_args(OptionParser(option_list = option_list)) 
+
+datafile <- opt$input
+chromosome=opt$chromosome
+userthread=opt$thread
+
+res = flamingo_main(hic_data=datafile, file_format='hic', domain_res=1e6, frag_res=5e3, chr_name=chromosome,normalization='VC',nThread=userthread)
+write.table(res, file=paste0("E12.5_FlamingoR_VC_", opt$chromosome, ".txt", sep=""), sep="\t", quote=F, row.names=F)
+
